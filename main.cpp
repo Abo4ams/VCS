b61699005c96367dd328aa08075cf834d09cf503
Mohamed Shams <68266191+Abo4ams@users.noreply.github.com>
2023-11-02 02:13:04 +0200

    Added getAverage Feature

--- main.cpp
+++ main.cpp
@@ -8,11 +8,22 @@ int getSum(std::vector<int> v){
     return sum;
 }
 
+double getAverage(std::vector<int> v){
+    int sum = 0;
+    double avg;
+    for(auto& i : v)
+        sum += i;
+    avg = (double) sum / v.size();
+    return avg;
+}
+
 int main() {
     std::vector<int> v;
     for(int i = 1; i <= 10; i++)
         v.push_back(i);
     int sum = getSum(v);
     std::cout << sum << std::endl;
+    double avg = getAverage(v);
+    std::cout << avg << std::endl;
     return 0;
 }
